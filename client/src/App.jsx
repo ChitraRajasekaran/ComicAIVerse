11e707bfe70248721f80bb7671b2658bfaed9946
Chitra Rajasekaran <crinspire01@gmail.com>
2024-05-07 22:47:58 -0400

    updated header to be responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,25 +6,26 @@ import {Home,CreatePost,Login, Showcase, CreateComic} from './pages';
 const App = () => {
   return (
     <BrowserRouter>
-     <header className='w-full flex justify-between items-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-        <Link to='/' className='text-4xl font-bold text-[#6449ff]'>
-          Comic<span className="text-[#ffbf00]">AIVerse</span>
-        </Link>
-        <nav className="space-x-4">
-        <Link to='/create-image' className='font-inter font-medium text-white px-4 py-2 rounded-md'>
+     <header className='w-full flex flex-col sm:flex-row justify-between items-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <Link to='/' className='text-3xl sm:text-4xl font-bold text-[#6449ff] mb-4 sm:mb-0'>
+        Comic<span className="text-[#ffbf00]">AIVerse</span>
+      </Link>
+      <nav className="space-y-2 sm:space-y-0 sm:space-x-4 text-center sm:text-left">
+        <Link to='/create-image' className='font-inter font-medium text-white px-4 py-2 rounded-md block sm:inline-block'>
           Generate Images
         </Link>
-        <Link to='/create-imageshowcase' className='font-inter font-medium text-gray-200 px-4 py-2 rounded-md'>
+        <Link to='/create-imageshowcase' className='font-inter font-medium text-gray-200 px-4 py-2 rounded-md block sm:inline-block'>
           Image Showcase
         </Link>
-        <Link to='/create-comic' className='font-inter font-medium text-yellow-200 px-4 py-2 rounded-md'>
+        <Link to='/create-comic' className='font-inter font-medium text-yellow-200 px-4 py-2 rounded-md block sm:inline-block'>
           Generate Comic Strips
         </Link>
-        <Link to='/login' className='font-inter font-medium text-pink-200 px-4 py-2 rounded-md'>
+        <Link to='/login' className='font-inter font-medium text-pink-200 px-4 py-2 rounded-md block sm:inline-block'>
           Login
         </Link>
-        </nav>
-      </header>
+      </nav>
+    </header>
+
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9f8fe] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path ='/' element ={<Home />} />
