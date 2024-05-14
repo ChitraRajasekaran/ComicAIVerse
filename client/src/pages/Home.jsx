ba9bb8ea15ea383c1f0b1c9298af9d23b252cccb
Chitra Rajasekaran <crinspire01@gmail.com>
2024-05-13 21:39:30 -0400

    Updated createStory with basic setups

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import video from '../assets/Final.mp4';
 
 const Home = () => {
   return (
-    <div class="h-full w-full min-h-screen bg-black">
+    <div className="h-full w-full min-h-screen bg-black">
       <section className="wallpaper-section relative flex flex-col items-center justify-center min-h-screen">
         <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50"></div>
         <div className="absolute inset-0 flex items-center justify-center">
@@ -22,10 +22,10 @@ const Home = () => {
           </h1>
         </div>
       </section>
-      <div class="descriptionContainer flex flex-col items-center mt-32 px-4">
-        <h2 class="descriptionPreTitle text-[#f5c74a] font-poppins text-base font-semibold uppercase">OUR COLLECTIONS</h2>
-        <h1 class="descriptionMainTitle text-center text-white font-poppins text-4xl font-semibold leading-normal tracking-wide">The AI-Powered Comics Creation</h1>
-        <p class="descriptionText text-center text-white text-opacity-80 max-w-md font-poppins text-lg font-medium leading-relaxed mt-8 mb-8">Transform your stories into comics, and share them with millions of comic enthusiasts worldwide.</p>
+      <div className="descriptionContainer flex flex-col items-center mt-32 px-4">
+        <h2 className="descriptionPreTitle text-[#f5c74a] font-poppins text-base font-semibold uppercase">OUR COLLECTIONS</h2>
+        <h1 className="descriptionMainTitle text-center text-white font-poppins text-4xl font-semibold leading-normal tracking-wide">The AI-Powered Comics Creation</h1>
+        <p className="descriptionText text-center text-white text-opacity-80 max-w-md font-poppins text-lg font-medium leading-relaxed mt-8 mb-8">Transform your stories into comics, and share them with millions of comic enthusiasts worldwide.</p>
       </div>
     </div>
   );
