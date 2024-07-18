6ffe231425db8c049b64705eef03444584d62892
Chitra <crinspire01@gmail.com>
2024-07-18 13:15:10 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,13 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['https://comic-ai-verse.vercel.app/'],
+        methods: ['POST','GET'],
+        credentials: true,
+    }
+));
 
 
 app.use(express.json({ limit: '50mb' }));
